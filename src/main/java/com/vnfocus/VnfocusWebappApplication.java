b1acfe63c2182c9d6a72a225f7b53566fecfa981
Viet Nguyen <vietnguyen@Viets-MacBook-Pro.local>
2017-04-22 18:38:33 +1000

    Initial projetc

---
+++ src/main/java/com/vnfocus/VnfocusWebappApplication.java
@@ -0,0 +1,12 @@
+package com.vnfocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnfocusWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnfocusWebappApplication.class, args);
+	}
+}
